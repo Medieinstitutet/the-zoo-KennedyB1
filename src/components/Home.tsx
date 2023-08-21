b2279f5f1e8914c81cb357b676341f36cb719969
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-21 15:57:38 +0200

    interface

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { IAnimal } from '../models/Ianimal'
+
+
+
 export const Home = () => {
 
 
