7400d561565f95ceb9928c80744c0f03bb1238ad
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-21 11:45:46 +0200

    Api

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 export const Home = () => {
+    interface Animal {
+        name: string;
+        shortDescription: string;
 
+    }
+    // https://restcountries.com/#endpoints-region
+    const [animal, setAnimal] = useState<Animal[]>([]);
 
-    return <h1>Home</h1>
-}
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get<Animal[]>(
+                    '/src/animalsData/animals.json'
+                );
+                const animalsData = response.data;
+                setAnimal(animalsData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        getData();
+    }, []);
+
+    return (
+        <div className="animals">
+            <h2>Animals</h2>
+            <ul>
+                {animal.map((animal, index) => (
+                    <li key={index}>
+                        <Link to={`${animal.name}`} >
+                            <h2>{animal.name}</h2> <h3>{animal.shortDescription}</h3>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
