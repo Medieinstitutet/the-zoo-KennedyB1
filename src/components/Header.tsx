0400a9b40849805a6066b2a5c4460cf70f499d7a
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-22 11:10:59 +0200

    Header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
 
-    return <><Link to={`/animals`}>
-        <h3>Animals</h3>
-    </Link></>
+    return <>
+        <Link to={`/`}><h3>Home</h3></Link>
+        <Link to={`/animals`}><h3>Animals</h3></Link></>
 
 }
